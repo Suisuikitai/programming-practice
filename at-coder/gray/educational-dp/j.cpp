7a483253b0677b9ef0bb8c86e2cb72b2e1ecaaee
Suisuikitai <mag.cupw@gmail.com>
2022-05-30 02:39:21 +0900

    もう一度最初から

--- at-coder/gray/educational-dp/j.cpp
+++ at-coder/gray/educational-dp/j.cpp
@@ -16,35 +16,3 @@ void chmin(T &a, T b)
 double dp[301][301][301];
 bool calculated[301][301][301];
 int N;
-double calculate(int plate1, int plate2, int plate3)
-{
-  if (calculated[plate1][plate2][plate3])
-    return dp[plate1][plate2][plate3];
-  //サイコロを振る期待値は最低でも1
-  //すしを1つ食べるためにはサイコロを少なくとも1回は振る必要があるから
-  //あとはそれからどれだけ増えるか
-  double ans = 1.0;
-}
-
-int main(int argc, char const *argv[])
-{
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  cin >> N;
-  vector<int> a(N + 1);
-  int plate1 = 0;
-  int plate2 = 0;
-  int plate3 = 0;
-  rep(i, N)
-  {
-    int sushi_quantity = 0;
-    cin >> sushi_quantity;
-    if (sushi_quantity == 1)
-      plate1++;
-    if (sushi_quantity == 2)
-      plate2++;
-    if (sushi_quantity == 3)
-      plate3++;
-  }
-  return 0;
-}
