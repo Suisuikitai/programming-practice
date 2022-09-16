793677d60b0e01e3d6b08532c247e0449e87f2b5
Suisuikitai <mag.cupw@gmail.com>
2022-09-17 02:51:01 +0900

    review

---
+++ at-coder/gray/educational-dp/template.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+read name
+filename="${name}.cpp"
+echo '#include <bits/stdc++.h>' >> $filename
+echo 'using namespace std;' >> $filename
+echo '#define rep(i, x) for (int i = 0; i < x; i++)' >> $filename
+echo '' >> $filename
+echo 'int main(int argc, char const *argv[])' >> $filename
+echo '{' >> $filename
+echo '  ios::sync_with_stdio(false);' >> $filename
+echo '  cin.tie(0);' >> $filename
+echo '	' >> $filename
+echo '	return 0;' >> $filename
+echo '}' >> $filename
